07a360be0b25a235323dd787f3d00bcc1edc3306
JuanCMA <47261168+JuanCMA@users.noreply.github.com>
2019-09-29 12:14:34 -0500

    FirstSimulation

---
+++ BotSimulationTalk-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=botsimulationtalk-Web-Deploy.pubxml -p:Password=eXubr6omkWryxgiup83FQrLGvWHrFtdxeLmYxsLbn6srtPiHNuJMoaF3wb6Z
+
